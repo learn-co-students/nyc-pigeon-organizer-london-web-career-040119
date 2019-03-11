f0f956b7d47d9d85d64d76d763a3479b1d141052
szib <szib77@gmail.com>
2019-03-11 19:39:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |attributum, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        result[pigeon] ||= {}
+        result[pigeon][attributum] ||= []
+        result[pigeon][attributum] << option.to_s
+      end
+    end
+  end
+  result
 end

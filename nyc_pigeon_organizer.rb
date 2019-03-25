348962d9dd02d7974033b907f505d83f25d5963a
Xabier C <xabier_abia@hotmail.com>
2019-03-25 19:59:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,16 @@
 
 def nyc_pigeon_organizer (hash)
   pigeon_list = {}
-  hash.each do |list, data|
-    data.each do |option, birds|
-      birds.each do |name|
+  hash.each do |a, b|
+    b.each do |c, d|
+      d.each do |name|
         if pigeon_list[name] == nil
           pigeon_list[name] = {}
         end
-        if pigeon_list[name][list] == nil
-          pigeon_list[name][list] = []
+        if pigeon_list[name][a] == nil
+          pigeon_list[name][a] = []
         end
-        pigeon_list[name][list] << option.to_s
+        pigeon_list[name][a] << c.to_s
       end
     end
   end

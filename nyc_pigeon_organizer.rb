6614d5f71234b72c6b294b8c8353238c54f77d55
Francisco Costa <francisco@fcosta.pt>
2019-03-27 11:58:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeons = []
+
+  data[:color].each do |color, birds|
+    birds.each {|pigeon| pigeons << pigeon}
+  end
+
+  pigeons.uniq
+
+  pigeon_list = {}
+
+  pigeons.each do |pigeon|
+    pigeon_list[pigeon] = {:color => [], :gender => [], :lives => []}
+  end
+
+  binding.pry
+
 end

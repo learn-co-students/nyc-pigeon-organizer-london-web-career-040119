4474585394e4389d891a38725e9375d46d05be39
github username <github email address>
2019-03-29 14:05:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    hash = {}
+    data.each do |stat, key|
+      key.each do |option, pigeons|
+        pigeons.each do |name|
+          hash[name] ||= {}
+          hash[name][stat] ||= []
+          hash[name][stat] << option.to_s
+        end
+      end
+    end
+
+    hash
 end

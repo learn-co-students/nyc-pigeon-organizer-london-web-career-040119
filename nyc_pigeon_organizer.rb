a86352c5c0330668b59e63f5549cefe66db87f89
github username <github email address>
2019-03-31 21:26:54 +0100

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |keys,hash|   #keys are color, gender and lives
+    hash.each do |inner_keys, arrs|
+      arrs.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name]={}
+        end
+        if !pigeon_list[name].has_key?(keys)
+          pigeon_list[name][keys]=[]
+        end
+        if !pigeon_list[name][keys].include?(inner_keys)
+          pigeon_list[name][keys] << inner_keys.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
